cac60bf7e15bcfead13e85d4dade8d9c3e5a3e6b
BlingblingJK <fjkzzcq@163.com>
2023-03-23 20:30:34 +0800

    add_webpack

---
+++ todolist/build/webpack.config.js
@@ -0,0 +1,9 @@
+const path = require("path");
+module.exports = {
+  mode: "development", // 开发模式
+  entry: path.resolve(__dirname, "../src/main.js"), // 入口文件
+  output: {
+    filename: "output.js", // 打包后的文件名称
+    path: path.resolve(__dirname, "../dist"), // 打包后的目录，不用新建，打包后自动生成
+  },
+};
--- todolist/vue.config.js
+++ todolist/vue.config.js
@@ -1,4 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  output: {
+      filename: '[name].[hash:8].js',      // 打包后的文件名称
+      path: path.resolve(__dirname,'../dist')  // 打包后的目录
+    }
 })
